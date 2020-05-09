87be92bcbb46b5276625796bea29a7061ba9246e
Andy Craze <accraze@gmail.com>
2020-05-09 10:49:45 -0700

    add async api

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+import random
+
+from quart import Quart, jsonify, request
+from service import Keyword_Spotting_Service
+
+app = Quart(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+async def predict():
+    # get audio file and save it
+    #print(request.files)
+    files = await request.files
+    audio_file = files['file']
+    file_name = str(random.randint(0, 100000))
+    audio_file.save(file_name)
+
+    kss = Keyword_Spotting_Service()
+    predict = kss.predict(file_name)
+
+    # remove the audio file
+    os.remove(file_name)
+
+    # send back the predicted keyword
+    data = {'keyword': predict}
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
